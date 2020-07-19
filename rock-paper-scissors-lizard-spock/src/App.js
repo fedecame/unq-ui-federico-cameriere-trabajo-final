3710fa03267bc598384621cef78730ca5d792a93
fedecame <fede.cameriere@gmail.com>
2020-07-19 17:01:37 -0300

    buena variedad de imagenes y efectos, empiezo con funcionalidad

--- rock-paper-scissors-lizard-spock/src/App.js
+++ rock-paper-scissors-lizard-spock/src/App.js
@@ -12,7 +12,6 @@ import paper from './images/paper.svg.png';
 import rock from './images/rock.svg.png';
 import scissors from './images/scissors.svg.png';
 import spock from './images/spock.svg.png';
-import roll from './images/roll.svg';
 import './styles/app.scss';
 
 function App() {
@@ -36,14 +35,15 @@ function App() {
         </Col>
       </Row>
       <Row>
-        {/* <Col><Image src={roll} alt="roll" style={{cursor:"pointer"}} className="hvr-radial-out" onClick={() => alert("asd")}/></Col> */}
-        <Col><a href="#" className="hvr-icon-grow"> texto <img src={roll} className="hvr-icon"/></a></Col>
-        <Col className="hvr-icon-pulse-grow"><Image src={lizard} alt="lizard" style={{cursor:"pointer"}} className="centered-img hvr-icon-color" roundedCircle/></Col>
-        <Col><div><Image src={paper} alt="paper" className="centered-img hvr-glow" roundedCircle/></div></Col>
-        <Col className="hvr-icon-grow"><Image src={rock} alt="rock" className="centered-img hvr-icon" roundedCircle/></Col>
-        <Col className="hvr-icon-bounce"><Image src={scissors} alt="scissors" className="centered-img hvr-icon"/></Col>
+        {/* <Col className="hvr-icon-pulse-grow"><Image src={lizard} alt="lizard" style={{cursor:"pointer"}} className="centered-img hvr-icon-color" roundedCircle/></Col> */}
+        <Col><Image src={lizard} alt="lizard" style={{cursor:"pointer"}} className="centered-img hvr-pulse-grow color-lizard" roundedCircle/></Col>
+        <Col><Image src={paper} alt="paper" className="centered-img hvr-glow color-paper" roundedCircle/></Col>
+        <Col><div className="hvr-radial-out"><Image src={rock} alt="rock" className="centered-img" roundedCircle/></div></Col>
+        {/* <Col className="hvr-icon-bounce"><Image src={scissors} alt="scissors" className="centered-img hvr-icon" roundedCircle/></Col> */}
+        <Col><Image src={scissors} alt="scissors" className="centered-img hvr-bounce-in color-scissors" roundedCircle/></Col>
         {/* <Col className="hvr-icon-fade"><Image src={spock} alt="spock" className="centered-img hvr-icon"/></Col> */}
-        <Col><div className="hvr-icon-grow"><Image src={spock} alt="spock" className="centered-img hvr-icon-color"/></div></Col>
+        {/* <Col><div className="hvr-icon-grow"><Image src={spock} alt="spock" className="centered-img hvr-icon-color"/></div></Col> */}
+        <Col><Image src={spock} alt="spock" className="centered-img hvr-grow color-spock" roundedCircle/></Col>
       </Row>
       <Row className="mt-5">
         <Col>
---
+++ rock-paper-scissors-lizard-spock/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return ( 
+        <>
+        <p>Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a></p>
+        <p>Icons made by <a href="https://www.flaticon.com/authors/darius-dan" title="Darius Dan">Darius Dan</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>
+        <p>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>
+        <p>Icons made by <a href="https://www.flaticon.com/authors/monkik" title="monkik">monkik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>
+        <p>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>
+        </>
+    );
+}
+ 
+export default Footer;
